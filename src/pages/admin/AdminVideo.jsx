4a6f696801011557608b4bb9640f185529c1192f
Rio.Dev <riowibowo0211@gmail.com>
2026-07-20 22:55:17 +0700

    fix: auto scroll to top on page navigation

--- src/pages/admin/AdminVideo.jsx
+++ src/pages/admin/AdminVideo.jsx
@@ -67,7 +67,6 @@ export default function AdminVideo() {
         />
 
         <input
-          required
           type="url"
           placeholder="Link YouTube (https://youtube.com/...)"
           className="w-full p-4 rounded-xl border border-slate-200 focus:ring-2 focus:ring-blue-500 outline-none"
